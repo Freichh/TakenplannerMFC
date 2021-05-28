5e99919b2f6ab73bffb27ad526e13540f1e36e32
Freek Heining <freek.heining@gmail.com>
2021-05-28 12:05:33 +0200

    Added note option to chores

--- TakenplannerData/Models/Chore.cs
+++ TakenplannerData/Models/Chore.cs
@@ -12,6 +12,7 @@ namespace TakenplannerData.Models
         public int Id { get; set; }
         public string Name { get; set; }
         public string Description { get; set; }
+        public string Note { get; set; }
 
         [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd-MM-yyyy}")]
         [DataType(DataType.Date)]
