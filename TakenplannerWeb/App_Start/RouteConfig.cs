1e736ca55ee856774cb868ac9a38f6672a38e980
Bram Jager <bram.jager@itvitaelearning.nl>
2021-06-03 10:40:41 +0200

    Created branch, Routing naar Chores

--- TakenplannerWeb/App_Start/RouteConfig.cs
+++ TakenplannerWeb/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace TakenplannerWeb
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Chores", action = "Index", id = UrlParameter.Optional }
             );
         }
     }
