e3f8bf3f66ee6202fbf37821231072d4aee91aa3
Bram Jager <bram.jager@itvitaelearning.nl>
2021-06-03 13:23:38 +0200

    Changed DateTime to european

--- TakenplannerData/Models/Chore.cs
+++ TakenplannerData/Models/Chore.cs
@@ -14,11 +14,11 @@ namespace TakenplannerData.Models
         public string Description { get; set; }
         public string Note { get; set; }
 
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd-MM-yyyy}")]
         [DataType(DataType.Date)]
         public DateTime StartDate { get; set; }
 
-        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:yyyy-MM-dd}")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd-MM-yyyy}")]
         [DataType(DataType.Date)]
         public DateTime EndDate { get; set; }
         public Status Status { get; set; }
